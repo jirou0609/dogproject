0c3319d98a4f4587ca2c0855e43b5e62f75cd815
jirou0609 <djangojirou0609@gmail.com>
2023-09-05 21:49:47 +0900

    20230905_2

--- dogapp/admin.py
+++ dogapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dogs
 
-# Register your models here.
+
+class DogsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dog_name')
+    list_display_links = ('id', 'dog_name')
+
+admin.site.register(Dogs, DogsAdmin)
--- dogapp/models.py
+++ dogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Dogs(models.Model):
+    dog_name = models.CharField(verbose_name='犬名', max_length=200)
+    image = models.ImageField(verbose_name='イメージ', upload_to='photos')
+    comment = models.TextField(verbose_name='コメント',)
+
+    def __str__(self):
+        return self.dog_name
+
