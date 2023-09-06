4285d58d95475b1c5e43e36ea571bae62abebc89
jirou0609 <djangojirou0609@gmail.com>
2023-09-06 21:40:30 +0900

    20230906_2

--- dogapp/urls.py
+++ dogapp/urls.py
@@ -5,4 +5,5 @@ app_name = 'dogapp'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('choiceform/', views.CreateAnswerView.as_view(), name='choiceform'),
 ]
--- dogapp/views.py
+++ dogapp/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
+from .forms import UserAnswerForm
 
 app_name = 'dogapp'
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+@method_decorator(login_required, name='dispatch')
+class CreateAnswerView(CreateView):
+    form_class = UserAnswerForm
+    template_name = 'choiceform.html'
+    success_url = reverse_lazy('dogapp:index')
+    def form_valid(self, form):
+        postdata = form.save(commit=False)
+        postdata.user = self.request.user
+        postdata.save()
+        return super().form_valid(form)
