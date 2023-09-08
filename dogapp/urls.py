55a080b9694da6c7ee22b541c07d444cd39e5859
jirou0609 <djangojirou0609@gmail.com>
2023-09-08 21:48:54 +0900

    20230908_3

--- dogapp/urls.py
+++ dogapp/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
-
 app_name = 'dogapp'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('choiceform/', views.CreateAnswerView.as_view(), name='choiceform'),
     path('dogs/<int:dog_id>/', views.dog_detail, name='dog_detail'),
+    path('question1/', views.question1, name='question1'),
+    path('question2/', views.question2, name='question2'),
+    path('question3/', views.question3, name='question3'),
+    path('question4/', views.question4, name='question4'),
+    path('question5/', views.question5, name='question5'),
+    path('question6/', views.question6, name='question6'),
+    path('create_answer/', views.CreateAnswerView.as_view(), name='create_answer'),
 ]
--- dogapp/views.py
+++ dogapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, CreateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
@@ -7,25 +7,12 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 
 from .forms import UserAnswerForm
-from .models import Choice
+from .models import Choice, UserAnswer
 
 app_name = 'dogapp'
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-
-# @method_decorator(login_required, name='dispatch')
-# class CreateAnswerView(CreateView):
-#     form_class = UserAnswerForm
-#     template_name = 'choiceform.html'
-#     success_url = reverse_lazy('dogapp:index')
-#     def form_valid(self, form):
-#         postdata = form.save(commit=False)
-#         postdata.user = self.request.user
-#         postdata.save()
-#         return super().form_valid(form)
-
-
 
 def calculate_similarity_with_order(list1, list2):
     total_elements = len(list1)
@@ -64,7 +51,47 @@ def find_most_similar_id_with_order(target_list, Choice):
 # else:
 #     print("一致するデータベース内のIDが見つかりませんでした。")
 
+def question1(request):
+    if request.method == 'POST':
+        answer1 = request.POST.get('answer')
+        request.session['answer1'] = answer1
+        return render(request, 'question2.html')
+    return render(request, 'question1.html')
 
+def question2(request):
+    if request.method == 'POST':
+        answer2 = request.POST.get('answer')
+        request.session['answer2'] = answer2
+        return render(request, 'question3.html')
+    return render(request, 'question2.html')
+
+def question3(request):
+    if request.method == 'POST':
+        answer3 = request.POST.get('answer')
+        request.session['answer3'] = answer3
+        return render(request, 'question4.html')
+    return render(request, 'question3.html')
+
+def question4(request):
+    if request.method == 'POST':
+        answer4 = request.POST.get('answer')
+        request.session['answer4'] = answer4
+        return render(request, 'question5.html')
+    return render(request, 'question5.html')
+
+def question5(request):
+    if request.method == 'POST':
+        answer5 = request.POST.get('answer')
+        request.session['answer5'] = answer5
+        return render(request, 'question6.html')
+    return render(request, 'question5.html')
+
+def question6(request):
+    if request.method == 'POST':
+        answer6 = request.POST.get('answer')
+        request.session['answer6'] = answer6
+        return redirect('create_answer')
+    return render(request, 'question6.html')
 
 @method_decorator(login_required, name='dispatch')
 class CreateAnswerView(CreateView):
@@ -73,21 +100,29 @@ class CreateAnswerView(CreateView):
 
 
     def form_valid(self, form):
-        # フォームから回答を取得
-        list_a = [
-            form.cleaned_data['answer_1'],
-            form.cleaned_data['answer_2'],
-            form.cleaned_data['answer_3'],
-            form.cleaned_data['answer_4'],
-            form.cleaned_data['answer_5'],
-            form.cleaned_data['answer_6']
-        ]
+        answer_1 = self.request.session.get('answer1')
+        answer_2 = self.request.session.get('answer2')
+        answer_3 = self.request.session.get('answer3')
+        answer_4 = self.request.session.get('answer4')
+        answer_5 = self.request.session.get('answer5')
+        answer_6 = self.request.session.get('answer6')
+
+        form_answer_1 = form.cleaned_data['answer_1']
+        form_answer_2 = form.cleaned_data['answer_2']
+        form_answer_3 = form.cleaned_data['answer_3']
+        form_answer_4 = form.cleaned_data['answer_4']
+        form_answer_5 = form.cleaned_data['answer_5']
+        form_answer_6 = form.cleaned_data['answer_6']
 
         # データベースからDogChoiceオブジェクトを取得
         dog_choices_from_db = Choice.objects.all()
 
         # 類似性を計算して最も類似したIDを見つける
-        most_similar_id = find_most_similar_id_with_order(list_a, dog_choices_from_db)
+        # most_similar_id = find_most_similar_id_with_order(list_a, dog_choices_from_db)
+        most_similar_id = find_most_similar_id_with_order(
+            [answer_1, answer_2, answer_3, answer_4, answer_5, answer_6],
+            dog_choices_from_db
+        )
 
         if most_similar_id is not None:
             print("フォームデータと最も一致率が高いデータベース内のID（順序考慮）:", most_similar_id)
@@ -104,9 +139,19 @@ class CreateAnswerView(CreateView):
             print("一致するデータベース内のIDが見つかりませんでした。")
 
         # 回答データを保存
-        postdata = form.save(commit=False)
-        postdata.user = self.request.user
-        postdata.save()
+        # postdata = form.save(commit=False)
+        # postdata.user = self.request.user
+        # postdata.save()
+
+        user_answer = UserAnswer(
+            answer_1=answer_1,
+            answer_2=answer_2,
+            answer_3=answer_3,
+            answer_4=answer_4,
+            answer_5=answer_5,
+            answer_6=answer_6,
+            user=self.request.user
+        )
 
         return super().form_valid(form)
 
